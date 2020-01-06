6f6eb59f18a54c1bb826edae309ac13ae1c32b6c
Allen Yang <allen.yang@ezprice.com.tw>
2020-01-07 00:33:30 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,35 +3,22 @@ from setuptools import setup, find_packages
 import sys
 
 setup(
-    name="ezselenium",
-    version="0.1.0",
-    author="Allen Yang",
-    author_email="musasi.yang@gmail.com",
-    description="Python Framework.",
-    license="MIT",
-    url="https://github.com/AllenYang0308",
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        "Environment :: Web Environment",
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: Chinese',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Topic :: NLP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    install_requires=[
-            'selenium>=3.141.0'
-    ],
-    zip_safe=True,
+    name='ezselenium',
+    version='0.1.0',
+    license='BSD',
+    author='Allen Yang',
+    author_email='allen.yang@ezprice.com.tw',
+    maintainer='IDV team',
+    description='',
+    packages=['binary', 'driver'],
     package_data={
         'binary': ['*.*', 'firefox/*'],
         'driver': ['*.*']
     },
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'selenium>=3.141.0'
+    ],
 )
