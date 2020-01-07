35bc1c4abe1c131d970c8fc0183c886fc2ffdea5
Allen Yang <allen.yang@ezprice.com.tw>
2020-01-07 10:34:53 +0800

    test

--- ezselenium/utils/browser.py
+++ ezselenium/utils/browser.py
@@ -12,4 +12,4 @@ def init_web_driver():
     options = Options()
     options.add_argument("-headless")
 
-    return webdriver.WebDriver(executable_path="firefox/geckodriver", firefox_binary="firefox/firefox", log_path=None, service_log_path=None, capabilities=FIREFOX, firefox_options=options)
+    return webdriver.WebDriver(executable_path="binary/firefox/geckodriver", firefox_binary="binary/firefox/firefox", log_path=None, service_log_path=None, capabilities=FIREFOX, firefox_options=options)
